639c94e02d2c23fdfb84207e5d7e30c4a8c44133
Vinod Suryawanshi <suryawanshiv505@gmail.com>
2023-09-29 00:34:29 +0530

    changes1

--- MainClass.java
+++ MainClass.java
@@ -2,5 +2,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello main class");
+System.out.println("Hello Vinod Check jenkin started or not ");
+		
 	}
 }
